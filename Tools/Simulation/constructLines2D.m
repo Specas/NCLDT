ba35cba54bfa56e0978b2e2565d440dba9c98239
specas <shrenik95@gmail.com>
2017-11-05 18:48:05 -0500

    Plotting functions

---
+++ Tools/Simulation/constructLines2D.m
@@ -0,0 +1,21 @@
+function [] = constructLines2D(ax, coord)
+
+%Function to join all the coordinates in coord. Each row of coord contains
+%an x and y coordinate. Lines are drawn between consecutive lines.
+%The first and last points are not joined. Hence a closed figure is not
+%formed. This is required for dynamic drawing for user visualization.
+
+%x and y coordinate
+x = coord(:, 1);
+y = coord(:, 2);
+
+%Drawing a line only if there is more than one point
+if size(x, 1)>1
+    for j=1:size(x, 1)-1
+        start_ind = j;
+        end_ind = j+1;
+        start_coord = [x(start_ind), y(start_ind)];
+        end_coord = [x(end_ind), y(end_ind)];
+        plot(ax, [start_coord(1), end_coord(1)], [start_coord(2), end_coord(2)], 'r');
+    end
+end
--- Tools/Simulation/createObstacles2D.m
+++ Tools/Simulation/createObstacles2D.m
@@ -3,21 +3,23 @@
 clc
 clear
 
-
+%Setting space limits and initializing figure
 size_x_min = -100;
 size_x_max = 100;
 size_y_min = -100;
 size_y_max = 100;
 
 figure('Name','2D Space','NumberTitle','off')
+xlabel('x');
+ylabel('y');
 ax = axes;
 xlim([size_x_min size_x_max]);
 ylim([size_y_min size_y_max]);
 grid on;
 
-
+%User input is taken for the obstacles and is dynamically drawn
 obstacle_coords = {};
-obstacle_count = 0;
+obstacle_count = 1;
 obstacle_current_coord = [];
 fprintf('Click on points to define obstacles.\nPress return to finish constructing the current obstacle.\nHit return without clicking to end obstacle creation.\n\n');
 
@@ -34,6 +36,8 @@ while true
         end
     else
         obstacle_current_coord = [obstacle_current_coord; [x, y]];
+        obstacle_coords{obstacle_count} = obstacle_current_coord;
+        
     end
     
     drawDynamicObstaclesClick2D(obstacle_coords, obstacle_count, ax);
