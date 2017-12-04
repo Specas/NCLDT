90e86a513162f2525dcba79952748560427dc1bd
jaymilindanjankar <anjankar@seas.upenn.edu>
2017-12-04 01:42:32 -0500

    Added multiple trees test code with global variables

--- NCLDT/createNewTree.m
+++ NCLDT/createNewTree.m
@@ -1,10 +1,44 @@
 %Function to create a new tree node
 
-function [] = createNewTree(T, Tm, path, wt, ws, wt_current, rho_current, alpha, epsilon_min, epsilon_max, eta, mu, size_eta, size_mu, m, q_pivot, q_end, obstacle_coords, ndim, lim)
 
-%Finding current number of trees
-num_trees = length(T);
+
+function [] = createNewTree(q_start,q_end, alpha_init,epsilon_max_init,epsilon_min_init,m_init,rho_init,obstacle_coords, ndim, lim)
+
+%Global parameters for each tree
+global T Tm path wt ws wt_current rho_current alpha  epsilon_min epsilon_max
+global eta mu eta_size mu_size
+global m q_root q_target q_pivot
+
+%Initializng paramters for each tree
+Tm{end+1} = [];
+path{end+1} = [];
+
+mu{end+1} = [];
+eta{end+1} = [];
+eta_size{end+1} = 0;
+mu_size{end+1} = 0;
 
 %Sampling new root node for the tree
-q_root = sampleConfigurationFree(obstacle_coords, ndim, lim);
+q_root{end+1} = sampleConfigurationFree(obstacle_coords, ndim, lim);
+T{end+1} = q_root{end};
+
+wt{end+1} = (q_end - q_root{end})/norm(q_end - q_root{end});
+ws{end+1} = -(q_start - q_root{end})/norm(q_start - q_root{end});
+
+q_target{end+1} = q_end;
+
+%Current direction of growth
+wt_current{end+1} = wt{end};
+
+%Variable along which you sample
+q_pivot{end+1} = q_root{end};
+
+%Current obstacle search radius=
+rho_current{end+1} = rho_init;
+
+alpha{end+1} = alpha_init;
+epsilon_min{end+1}= epsilon_min_init;
+epsilon_max{end+1}= epsilon_max_init;
+m{end+1} = m_init;
+end
 
---
+++ NCLDT/testMultipleNCLDT2D.m
@@ -0,0 +1,72 @@
+%Function to generate multiple trees
+%Test file to run test NCLDT modules (2D)
+clear
+clc
+close all
+
+%Adding path
+addpath(genpath('..\Tools\'));
+
+%Docking figure
+set(0,'DefaultFigureWindowStyle','docked');
+
+size_x_min = -100;
+size_x_max = 100;
+size_y_min = -100;
+size_y_max = 100;
+
+
+
+lim = [size_x_min, size_x_max; size_y_min, size_y_max];
+ndim = 2;
+
+%Creating Obstacle in space
+[fig, ax] = initializeFigure2D('2D Space', 'GridOn', [size_x_min size_x_max], [size_y_min, size_y_max]);
+% [fig, ax, obstacle_coords] = createObstacles2D(fig, ax);
+load('obstacle_coords.mat');
+
+%Draw filled obstacles
+[fig, ax] = drawObstacles2D(fig, ax, obstacle_coords, 'Filled');
+
+%Initialize cell for storing elements related to tree
+
+%Global parameters for each tree
+global T Tm path wt ws wt_current rho_current alpha  epsilon_min epsilon_max
+global eta mu eta_size mu_size
+global m q_root q_target q_pivot
+
+%qtarget remains constant. qend will change if a tree is connected as the
+%new sample points will try connecting to qtarget or exisitng trees.
+
+%NCLDT parameters
+alpha_init = 45*pi/180;
+epsilon_max_init = 10;
+epsilon_min_init = 3;
+m_init = 5;
+rho_init = 0.1;
+k1 = 10^5;
+k2 = 10^-5;
+k3 = 5;
+iteration=0;
+
+%Initializing tree parameters as empty cells
+T={}; Tm={}; path={}; wt={}; ws={}; wt_current={}; rho_current={}; alpha={};
+epsilon_min={}; epsilon_max={};
+eta={}; mu={}; eta_size={}; mu_size={};  m={}; q_root={}; q_target ={}; q_pivot={};
+
+%Specify number of initial trees
+num_trees=5;
+
+%Selecting the start and end configurations
+fprintf('Click to select the start configuration.\n');
+fprintf("%d", i);
+q_start = setConfiguration2D(fig, ax);
+
+fprintf('Click to select the end configuration.\n');
+q_end = setConfiguration2D(fig, ax);
+
+for i=1:num_trees
+    createNewTree(q_start,q_end, alpha_init,epsilon_max_init,epsilon_min_init,m_init,rho_init,obstacle_coords, ndim, lim);
+end
+
+
