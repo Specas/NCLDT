6a1fa6e6c1c179c4181361f424ecc860d10a049a
jaymilindanjankar <anjankar@seas.upenn.edu>
2017-12-05 00:36:51 -0500

    is configInFree changes reverted to og

--- Tools/Mechanics/isConfigInFree2D.m
+++ Tools/Mechanics/isConfigInFree2D.m
@@ -10,7 +10,7 @@ function [free] = isConfigInFree2D(q, obstacle_coords, lim)
 free = true;
 
 %Checking for configuration space limits
-if q(1,1)< lim(1,1) | q(1,1)> lim(1,2) | q(1,2)< lim(2,1) | q(1,2)>lim(2,2)
+if q(1)< lim(1,1) | q(1)> lim(1,2) | q(2)< lim(2,1) | q(2)>lim(2,2)
     free = false;
 end
 
